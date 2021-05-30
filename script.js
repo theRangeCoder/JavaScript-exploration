b3a6ad72f0812252d13df880731aa14f0e6e27f3
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-05-30 23:14:23 +0200

    Creation, initialization and display of variables

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 // Declaring a variable
 let firstName = "Shuva";
 
-// Printing the variable
+// Declaration and initialization separately
+let lastName;
+lastName = 'Bhual';
+
+// Printing the variables
 console.log(firstName);
+console.log(firstName + " " + lastName);
+
+// Declaring integers
+let a = 5;
+console.log(a);
+
+// Declaring booleans
+let isEmployed = true;
+isEmployed = false;
+
+console.log(firstName + a);
+
+// Experimenting with integers, declared as strings
+b = '6';
+console.log(a+b);
+console.log(a+a);
+
+// Challenge: It your Dog's Birthday!
+
+/*
+ Create 2 variables related to your dog. Make one of them its age and update it by one.
+ Log out all the values in a friendly way of your choosing.
+*/
+let name;
+name = 'Monty';
+let age = 5;
+age = age+1;
+console.log('Name: '+name);
+console.log('Age: '+age);
