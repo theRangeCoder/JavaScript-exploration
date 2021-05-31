bebe307b47e4bbd2ee9cf7379e69317ac0f2a8fb
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-05-31 21:21:32 +0200

    Experimented with some string functions such as 'toUpperCase()', 'trim()' and 'split()'

--- script.js
+++ script.js
@@ -38,11 +38,30 @@ console.log('Age: '+age);
 
 // Declaring variables using const (Can neither be redeclared nor updated)
 const a2 = 5; 
-console.log(a);
+console.log(a2);
 
 var temp1; // Initialized as 'undefined'
 let temp2; // Initialized as 'undefined'
 const temp3=10; // 'const' should be assigned a value, at the time of declaration
 console.log(temp1+" "+temp2);
 
+// Strings in JavaScript
+ let str1 = "Munich";
+ console.log(typeof str1);
 
+ // Concatenating strings (the curly braces could be used as one of the methods)
+console.log(`${firstName} ${lastName} ${a}`);
+
+// Some string methods
+console.log(firstName.length);
+firstName = firstName.toUpperCase();
+console.log(firstName);
+
+// THe 'split()' function
+let str2 = "The quick brown fox jumps over the lazy dog";
+arr1 = str2.split(' '); // splits the words of the strings at each empty space and stores them in an array
+console.log(arr1);
+
+// The 'trim()' function - removes all spaces before and after a string
+str3 = str2.trim();
+console.log(str3.length);
