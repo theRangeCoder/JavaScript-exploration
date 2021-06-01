1ad6001bf837ad98d78f00bff97366f4b5d61f30
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-01 21:12:28 +0200

    Iterated through the array using forEach()

--- script.js
+++ script.js
@@ -111,3 +111,19 @@ console.log(arr2);
 // Removing the last value of an array (the 'pop()' function)
 arr2.pop();
 console.log(arr2);
+
+// Iterating through the array ('forEach' statement)
+arr2.forEach((element) => {
+    console.log(element)
+})
+
+// Challenge - Manage Inventory
+
+/*
+    1. Your company is launching 3 new products: toilet paper, bottled water, and sanitizer. Store them in a list.
+    2. Turns out there was a mistake and toilet paper was actually paper towels. Make the appropriate update.
+    3. Sanitizer sells out. Remove it from the list.
+    4. Business is so good the company launches a new product: Bleach. Add it to the list.
+    
+    Note: After creating the initial array do not just create a brand new array. Modify it accordingly.
+*/
