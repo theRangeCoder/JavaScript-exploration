334da22d87e96f51163b269d5f52390b393ff2b4
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-03 11:22:39 +0200

    Completed a short challenge, related to objects in JavaScript

--- script.js
+++ script.js
@@ -160,3 +160,26 @@ console.log(obj1.firstName);
 
 console.log(Object.values(obj1));
 console.log(obj1.hasOwnProperty('firstName'));
+
+/* Challenge - User Profile Form
+1. Your organization needs the user's personal information to ship the inventory to. Create an object to store the data in.
+2. Store a user's first and last name.
+3. Store a user's address. City and state will be enough. Have this as a nested object. (Interface Segregation Principle)
+4. The business wants to give more personalized ads and wants to collect information about the user's hobbies. Create a list of hobbies.
+5. Are they a gold member? Store a boolean with this value.
+*/
+let user = {
+    fName: 'John',
+    lName: 'Smith',
+    address: {
+        city: 'Houston',
+        state: 'Texas'
+    },
+    hobbies: ['Singing', 'Dancing', 'Writing short stories'],
+    goldMember: true
+};
+console.log(user.lName);
+console.log(user.address.city);
+console.log(user.hobbies[1]);
+console.log(user);
+//  To add another property: value pair, we could just type: user.property = value;
