f4a78b4991f67d33c9c8dc416de48ffbe135b534
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-04 23:31:51 +0200

    if/ elseif/ else in JS

--- script.js
+++ script.js
@@ -267,3 +267,35 @@ if(choice==true) {
     quantity++;
 }
 console.log("Updated quantity: "+quantity);
+
+// Flow of control
+let num1=5;
+if (num1%2 === 0) {
+    console.log(num1+" is an even number");
+}
+else {
+    console.log(num1+" is an odd number");
+}
+
+// and = && and or = ||
+// The first statement/ condition, which is evaluated to be true, is the one which is run.
+
+// Challenge 1: User Age
+
+/*
+ 1. Our business wants to keep track of what age range a user falls into.
+ 2. If they are 12 or younger log 'child'.
+ 3. If not a child but the user's age is under 20 log 'teenager'.
+ 4. If neither log 'adult'.
+*/
+
+const age2 = 20;
+if (age2<=12) {
+    console.log('child');
+}
+else if (age2<20) {
+    console.log('teenager');
+}
+else {
+    console.log('adult');
+}
