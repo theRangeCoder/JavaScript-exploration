5be2bbc01ece29425f4af07e57aafc34b133b588
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-08 23:30:43 +0200

    Functions in JavaScript (the challenge)

--- script.js
+++ script.js
@@ -458,3 +458,38 @@ function add3(num1,num2) {
     return(num1+num2);
 }
 console.log(add3(5,6));
+
+// Challenge: Checkout
+
+/*
+ 1. Go through the full checkout process.
+ 2. Use getUserCredentials provided that returns the first name, last name and concats with email.  
+ 3. Write a function called getPreTaxTotal that take in our cartItems and returns the total without tax.
+ 4. Pass the preTaxTotal value to a getTaxedTotal function that returns the value with tax.
+ 5. Log each value from the 3 functions out. 
+*/
+console.log(cartItems);
+
+function getUserCredentials() {
+    let firstName = 'Shuva';
+    let lastName = 'Bhual';
+    let email = 'shuvabhual@email.com';
+
+    return {firstName, lastName, email};
+}
+
+function getPreTaxTotal(cartItems) {
+    let total=0;
+    for (let item of cartItems) {
+        total+=item.quantity*item.price;
+    }
+    return total;
+}
+
+function getTaxedTotal(preTaxTotal) {
+    return preTaxTotal*1.08;
+}
+
+console.log(getUserCredentials());
+console.log("Pre-tax total: "+getPreTaxTotal(cartItems));
+console.log("Price after tax: "+getTaxedTotal(getPreTaxTotal(cartItems)));
