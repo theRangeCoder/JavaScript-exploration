2cd4fbaaf92d8e098d5936acebee55cb5bd0a359
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-15 23:19:58 +0200

    Strings in JavaScript

---
+++ script2.js
@@ -0,0 +1,10 @@
+// Strings in JavaScript
+let username = "Shuva";
+console.log(username);
+
+// Create a variable, message, that stores the string: "You have three new notifications"
+let message = "You have three new notifications";
+
+// Combine the above two strings
+let combinedStr = message+", "+username;
+console.log(combinedStr);
