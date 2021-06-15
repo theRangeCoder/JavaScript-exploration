99496ed4ad4a5035043b449ccb10395a0e3e7494
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-15 23:35:10 +0200

    Completed a challenge using strings in JavaScript

--- script2.js
+++ script2.js
@@ -8,3 +8,13 @@ let message = "You have three new notifications";
 // Combine the above two strings
 let combinedStr = message+", "+username;
 console.log(combinedStr);
+
+/* Create two variables, name and greeting. The name variable should store your name, and the greeting should store e.g. "Hi, my name is ".
+Create a third variable, myGreeting, that contatenates the two strings.
+Log myGreeting to the console */
+let name = "Shuva";
+let greeting = "Hi, my name is ";
+let myGreeting = greeting+name;
+console.log(myGreeting);
+
+// NOTE: When one concatenates a number with a string, the output is always a string by default.
