899f769fe3ba3fc3560df10d3030f6873e90c93e
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-16 23:16:47 +0200

    Added a greeting message to the inner text of the paragraph element

--- script2.js
+++ script2.js
@@ -18,3 +18,16 @@ let myGreeting = greeting+name;
 console.log(myGreeting);
 
 // NOTE: When one concatenates a number with a string, the output is always a string by default.
+
+/* Challenge:
+Grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+Create two variables (name2 & greeting2) that contains your name and the greeting we want to render on the page
+
+Render the welcome message using welcomeEl.innerText 
+*/
+let welcomeEl = document.getElementById("welcome-el");
+let name2 = "Shuva";
+let greeting2 = "Welcome back";
+
+welcomeEl.innerText = greeting2 + ", " + name2;
