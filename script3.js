c1ff0d7ca408bd637e585909f8b5790d78a1165b
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-07-25 20:47:45 +0200

    Destructuring objects

--- script3.js
+++ script3.js
@@ -2,4 +2,29 @@
 document.getElementById("temp-lit").innerText = `Hello
 there!`;
 
+// Destructuring objects (accessing the keys of an object easily by breaking the object into its component keys)
+let ob1 = {
+    name: "John",
+    age: 23,
+    address: {
+        city: "Paris"
+    }
+};
 
+const {name, age, address: {city}} = ob1;
+
+console.log(name);
+
+// Example: Destructure the following object
+const student = {
+    name2: "Kyle",
+    age2: 24,
+    projects: {
+        diceGame: "Two player dice game using JavaScript"
+    }
+}
+
+const {name2, age2, projects: {diceGame}} = student;
+
+console.log(name2);
+console.log(diceGame);
