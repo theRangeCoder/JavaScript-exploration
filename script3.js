165d322bc7609bf7a43b48e3414b30921fe20394
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-07-27 22:06:12 +0200

    for of loop

--- script3.js
+++ script3.js
@@ -49,3 +49,21 @@ function addressMaker(address) {
 }
 
 addressMaker({city: 'Austin', state: 'Texas'});
+
+// for of loop (can be used for any iterable - an array, a string, etc)
+
+// Challenge - For Of Loop
+
+// Using the For of Loop, iterate through the array and print into the console, a message like:
+// Tom lives in Lisbon
+
+const students = [ 
+    { name: "John", city: "New York" },
+    { name: "Peter", city: "Paris"},
+    { name: "Kate", city: "Sydney" }
+]
+
+for (const obj of students) {
+    const {name, city} = obj;
+    console.log(`${name} lives in ${city}`);
+}
