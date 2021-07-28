193dbee5e0867ebb865b83dd2648f3fed3eea9e3
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-07-28 19:55:43 +0200

    The spread operator

--- script3.js
+++ script3.js
@@ -67,3 +67,22 @@ for (const obj of students) {
     const {name, city} = obj;
     console.log(`${name} lives in ${city}`);
 }
+
+// The spread operator (...) (to store a copy of the values of an array/ object in another array/ object)
+// Usage: newArr = [...oldArr] and newObj = {...oldObj}
+
+/*
+    **** Challenge ****
+    
+    Imagine you are going out to do some grocery shopping.
+    So you have an array called shoppingList with all the products you want to buy.
+    
+    Now that you are inside of the shop, you have a basket with all the products from your list, but you want to add a few more.
+    
+    Create a new array called shoppingBasket, that will be a copy of the shoppingList array, and add some new products into it. 
+
+*/
+
+const shoppingList = ["eggs", "milk", "butter"];
+let shoppingBasket = [...shoppingList, "bread", "sugar", "salt", "tomatoes"];
+console.log(`The new shopping list: ${shoppingBasket}`);
