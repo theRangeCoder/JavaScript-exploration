a90f8adc72569f0a63f5386d8777a5402d1d8f95
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-08-01 22:56:04 +0200

    Default parameters

--- script3.js
+++ script3.js
@@ -129,3 +129,24 @@ const oddOrEven = (n) => {
     }
     return "Even";
 }
+
+// Default parameters (const nameOfFunction = (parameter1 = default parameter 1, parameter2 = default parameter 2, ...) => {...})
+/*
+**** Challenge *****
+
+Create a function that receives a parameter of food.
+If your parameter food is going to have a value of "milk"
+the function should print into the console the following:
+
+"I'm going to buy milk from the grocery shop"
+
+But if you dont pass a value to your parameter food, it should print
+"I'm going to buy something from the grocery shop"
+
+*/
+const buyItem = (food = "something") => {
+    console.log(`I'm going to buy ${food} from the grocery shop`);
+}
+
+buyItem("milk");
+buyItem("something");
