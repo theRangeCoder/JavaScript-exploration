c451fa6cf2dfa5e8328f9c808c947fc8041cd672
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-08-05 17:19:14 +0200

    Created the Animal class with two initial functions

--- data.js
+++ data.js
@@ -2,3 +2,21 @@
 export const add4 = (n1,n2) => {
     return n1+n2;
 }
+
+export class Animal {
+    constructor(type, legs) {
+        // the 'this' keyword is used to refer to a particular instance of a class
+        this.type = type;
+        this.legs = legs;
+    }
+
+    // Creating a function of the class (The 'function' keyword is not required in this case)
+    meow(sound) {
+        console.log(sound);
+    }
+
+    // Creating a static function of the class (It can be invoked without creating an instance of the class)
+    static message() {
+        console.log(`Welcome to the Animal class`);
+    }
+}
