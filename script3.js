ffe22a9e41451a2b874bf026bbd74d922b682118
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-08-07 23:40:15 +0200

    Promises

--- script3.js
+++ script3.js
@@ -250,3 +250,30 @@ setTimeout(callback2(), 6000); // here, we use 'callback2()' with the brackets, 
 
 // Event listeners are also an example of asynchronous programming because the callback function only gets executed when an action is performed by the user.
 
+// 2. Promises
+/**
+ * Promises - Challenge
+ * Create a promise that returns some user data and throws an error when not found.
+ * Log the user data when listening to the promise as well as log the error.
+ * 
+ * Docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+*/
+
+const userData = new Promise((resolve, reject) => {
+    const error = false;
+    
+    if(error) {
+        reject('Data not found'); // define a string
+    } else {
+        resolve({ // define an object
+            firstName: 'Shuva',
+            age: 23,
+            email: 'ShuvasEmail@gmail.com'
+        });
+    }
+});
+
+userData
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+
